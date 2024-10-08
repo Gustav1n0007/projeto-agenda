14d7b0ee4b0055828b6379960592a1cfd73e9a6e
inserir nome <guilherme.mitsuyuki5546@gmail.com>
2024-10-08 08:44:46 -0300

    criaçao da tela de login

---
+++ frmLogin.cs
@@ -0,0 +1,15 @@
+namespace projeto_agenda
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
